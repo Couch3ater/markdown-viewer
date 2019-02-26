e5be70c4d5a828872406c8302e2af1f6c111acbd
Couch3ater <Couch3ater@users.noreply.github.com>
2019-02-26 09:22:56 -0500

    made page SFW

--- src/Info.js
+++ src/Info.js
@@ -4,7 +4,7 @@ const Info = (props) => {
 	return(
 		<div id='info'>
 			<div className='content'>
-				<h1>Couch3ater's Simple Markdown Shits.</h1>
+				<h1>Couch3ater's Simple Markdown Viewer.</h1>
 				<p>This application is nothing more than a simple markdown editor. Input is previewed in real-time as information is added to the text editor. Support for Github-flavored markdown will be added at a later date.</p>
 			</div>
 		</div>
